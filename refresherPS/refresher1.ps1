e2d29fc4222357ae444fd687c2547d7327b783b1
bolaji <31362517+ibrolord@users.noreply.github.com>
2018-05-04 10:39:19 -0400

    Add files via upload

---
+++ refresherPS/refresher1.ps1
@@ -0,0 +1,53 @@
+﻿Get-CIPolicy
+dir . -Recurse
+Get-Alias
+help Set-Alias -Detailed
+echo "sooodddd `
+sddsdsds"
+
+echo @"
+I am
+chillin
+"@
+&{ 1 + 1 }
+
+$test=1
+. { $test += 1 }
+& { $test += 1 }
+
+$test1 = [scriptblock]::Create("1+1")
+$test1
+&$test1
+
+([int] "1").ToString().GetType()
+
+("what about","an array").GetType()
+$ar=@("sd","ty","rr")
+$ar | measure-object -sum |  Sum
+
+1..5 | %{
+    "Recieved $_"
+    }
+
+foreach ($ar in @(1..5)){
+    "Recieved $ar"
+    }
+
+
+$env
+
+cd env:
+ls
+Set-Item COMPUTERNAME "Bro"
+
+ls c:/ | clip
+notepad
+
+$files = Get-ChildItem C:\Windows
+$files | measure-object
+$files | ConvertTo-Csv | Set-Content C:\demo\bra.csv
+
+$file1 = Get-Content C:\demo\bra.csv | ConvertFrom-csv
+
+$file1 | Select-Object -Property Name, Attributes | ConvertTo-Csv | Set-Content C:\demo\bra.csv 
+
---
+++ refresherPS/refresher2.ps1
@@ -0,0 +1,42 @@
+﻿rename-computer Naae
+restart-computer
+Stop-Computer
+Get-NetIPConfiguration
+new-netipaddress -InterfaceAlias ethernet -IPAddress 10.0.0.0 -PrefixLength 24 -DefaultGateway 192.0.0.1
+set-dnsclientserveraddress -InterfaceAlias Ëthernet -ServerAddresses 192.0.0.1
+Add-Computer -DomainName ff.loc
+
+Get-NetAdapterStatistics
+Test-NetConnection 
+Test-Connection google.com -port 25
+dfff
+echo $?
+
+Test-ComputerSecureChannel -Credential domain\admin -Repair #fix trust error
+get-eventlog -LogName System -EntryType Error
+
+Get-Service | Where-Object {$_.Status -eq "stopped"}
+
+install-windowsfeatures
+-IncludeAllsubfeature
+-IncludeManagementTools File-Services
+
+Install-WindowsFeature Net-Framework-Core -source d:\
+
+Get-HotFix #check installed updates
+
+New-NetFirewallRule -displayname "Allow Inbound Port 80" -Direction Inbound -LocalPort 80 -Protocol TCP -Action Allow #block rplace allow with block
+
+$pwd = ConvertTo-SecureString -String "Password01" -AsPlainText -Force
+new-aduser -name test.user -accountpassword $pwd
+enable-adaccount -identity test.user
+
+set-adaccountpassword test.user -newpassword $pwd -reset -passthru | set-aduser -changepasswordatlogon $true 
+
+new-adgroup -name "test" -samaccountname test -groupcategory security -groupscope global -path "CN=users,DC=IgniteNZ,DC=Internal"
+
+search-adaccount -passwordneverexpire #-accountinactive -timespan 90.00:00:00 # -lockedout # -accountdisabled 
+
+New-IseSnippet -Force -Title "Password_String" -Description "Secure Password String" -Text "`$newpwd = ConvertTo-SecureString -String P@ssw0rd -AsPlainText -Force"
+
+add-dnsserverpri
